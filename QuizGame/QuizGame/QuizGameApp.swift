ecd0bd0d536ef2a6fae08e6456dac09125ccc20a
jaelyung <jaekim7724@gmail.com>
2023-12-18 23:28:32 +0900

    [chore] 프로젝트 최초생성

---
+++ QuizGame/QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by jaelyung kim on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
